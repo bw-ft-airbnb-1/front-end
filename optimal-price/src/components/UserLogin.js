a4b0697b81c72b09616403c9f080581d513ef46e
imxande <alerodriguezlarosa@gmail.com>
2020-01-07 14:38:14 -0500

    pulled from dev branch to get changes made

--- optimal-price/src/components/UserLogin.js
+++ optimal-price/src/components/UserLogin.js
@@ -101,31 +101,12 @@ const FormikUserLogin = withFormik({
             setStatus(response.data.user);
 
            props.history.push('/dashboard');
-            // resetForm();
+            
         })
         .catch((error)=> {
             console.log("This is an async error:", error)
         })
   }
-
-//   handleSubmit: e => {
-//     e.preventDefault();
-//     axiosWithAuth()
-//                   .post(`https://bw-ft-airbnb-1.herokuapp.com/api/v1/user/signin `, credentials)
-//                   .then(response =>{
-//                     localStorage.setItem('token', response.data.token)
-//                     localStorage.setItem("email", credentials.email);
-//                     // setting state
-//                     setCredentials({
-//                       email: credentials.email
-//                   });
-//                     // props.history.push("/dashboard")
-//                   })
-//                   .catch(error => console.log(error))
-// }
-
-
-
 
 })(UserLogin)
 
