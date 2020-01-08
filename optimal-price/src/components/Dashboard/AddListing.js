1909b5bb339db35ce7b87956d063d9e1a4a954cd
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-01-08 18:17:08 -0500

    Day 3 checkpoint

--- optimal-price/src/components/Dashboard/AddListing.js
+++ optimal-price/src/components/Dashboard/AddListing.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { NavBar } from '../NavBar.js';
+import axios from "axios";
+
 
 import {
     Jumbotron,
@@ -11,28 +13,44 @@ import {
  
 
   import '../Dashboard.css';
-import { reset } from 'ansi-colors';
 
- // import {axiosWithAuth} from './../utils/axiosWithAuth';
+
+ import {axiosWithAuth} from '../../utils/axiosWithAuth';
 //import Context from './../contexts/loginContext';
 
 export const AddListing = () => {
  const [listing, setListing] = useState({
-     minimumPrice: "",
-     minimumNights: "",
+     price: "",
+     minimum_nights: "",
      bedrooms: "",
      bathrooms: "",
      accomodates: "",
-     bedType: "",
-     securityDeposit: "",
-     roomType: "",
-     propertyType: "",
-     zipCode: "",
+     bed_type: "",
+     security_deposit: "",
+     room_type: "",
+     property_type: "",
+     zip_code: "",
      amenities: {"laundry":false, "spa":false, "wifi":false, "cleaning":false, "breakfast":false, "security":false, "pool":false, "patio":false, "pets":false}
  })
+
  const [priceEst, setPriceEst] = useState(0)
 
- // Still need to take care of Amenities, entirePlace, and Availability
+//  const [error, setError] = useState({
+//         bedroomError: "",
+//         bathroomError: "",
+//         securityDepositError: "",
+//         zip_code_error: ""
+//  })
+
+//  useEffect(()=>{
+//     axiosWithAuth().get('/properties/getOptions')
+//     .then(res =>{
+//         console.log(res)
+//     }).catch(err => {
+//         console.log(err)
+//     })
+//  },[])
+
 
  const handleChange = (event) => {
      setListing({
@@ -52,6 +70,8 @@ const updateCheckbox = (event) => {
 }
 
 
+
+
  const addListing = (event) => {
      event.preventDefault()
      console.log("This is your add listing form state:", listing)
@@ -61,47 +81,70 @@ const updateCheckbox = (event) => {
 const runPriceEstimator = (event) => {
     event.preventDefault()
     setPriceEst(
-        listing.minimumPrice * listing.minimumNights
+        listing.price * listing.minimum_nights
     )
 }
 
 const cancelForm = () => {
     setListing({
-        minimumPrice: "",
-        minimumNights: "",
+        price: "",
+        minimum_nights: "",
         bedrooms: "",
         bathrooms: "",
         accomodates: "",
-        bedType: "",
-        securityDeposit: "",
-        roomType: "",
-        propertyType: "",
-        zipCode: "",
+        bedtype: "",
+        security_deposit: "",
+        room_type: "",
+        property_type: "",
+        zip_code: "",
         amenities: {"laundry":false, "spa":false, "wifi":false, "cleaning":false, "breakfast":false, "security":false, "pool":false, "patio":false}
 
     })
     setPriceEst(0.00)
+
+    // setError({
+    //     bedroomError: "",
+    //     bathroomError: "",
+    //     securityDepositError: "",
+    //     zip_code_error: ""
+    // })
 }
+
+// const validate = () => {
+//     let bedroomError = ""
+//     let bathroomError = ""
+//     let securityDepositError = ""
+//     let zip_code_error = ""
+    
+//     // bedroom validation
+    
+//     // bathroom validation
+
+//     // security deposit validation
+
+//     // zip code validation
+//     } 
+
 
     return(
         <div>
         <NavBar />
         <Card id="output-card" className="addCard">
-    <h1 className="output-label-text">Predicted Price: $ {priceEst}</h1>
+    <h1 className="output-label-text">Suggested Listing Price: $ {priceEst}</h1>
         </Card>
             <Jumbotron className="addListing-jumbo">
                 <form className="addListing" onSubmit={AddListing}>
 
                     <Card className="addCard">
                         <label className="addListing-label">
-                        <h4 className='label-text'>Minimum Price: </h4>
+                        <h4 className='label-text'>Price: </h4>
                             <input
                                 type="number"
-                                name="minimumPrice"
-                                value={listing.minimumPrice || ""}
+                                name="price"
+                                value={listing.price || ""}
                                 onChange={handleChange}
                                 min="0"
-                                placeholder="Min. Price"
+                                placeholder="Price"
                                 onChange={handleChange}
                                 required
                             />
@@ -112,8 +155,8 @@ const cancelForm = () => {
                             <h4 className='label-text'>Minimum Nights: </h4>
                             <input
                             type="number"
-                            name="minimumNights"
-                            value={listing.minimumNights || ""}
+                            name="minimum_nights"
+                            value={listing.minimum_nights || ""}
                             onChange={handleChange}
                             min="1"
                             placeholder="Min. Nights"
@@ -167,18 +210,18 @@ const cancelForm = () => {
                         <label className="addListing-label">
                         <h4 className="label-text">Bed Type: </h4>
                             <select
-                            name="propertyType"
-                            value={listing.bedType}
+                            name="bed_type"
+                            value={listing.bed_type}
                             onChange={handleChange}
                             required
                             >
                                 <option value="none">Bed Type</option>
-                                <option value="realBed">Real Bed</option>
-                                <option value="pullOut">Pull-out</option>
+                                <option value="real_bed">Real Bed</option>
+                                <option value="pull_out">Pull-out</option>
                                 <option value="sofa">Sofa</option>
                                 <option value="futon">Futon</option>
                                 <option value="couch">Couch</option>
-                                <option value="airbed">Airbed</option>
+                                <option value="air_bed">Airbed</option>
                             </select>
                         </label>
                     </Card>
@@ -188,9 +231,9 @@ const cancelForm = () => {
                             <input
                             type="number"
                             min="0"
-                            name="securityDeposit"
+                            name="security_deposit"
                             placeholder="Security Deposit"
-                            value={listing.securityDeposit}
+                            value={listing.security_deposit}
                             onChange={handleChange}
                             required
                             />
@@ -200,14 +243,14 @@ const cancelForm = () => {
                         <label className="addListing-label">
                         <h4 className="label-text">Room Type: </h4>
                             <select
-                            name="roomType"
-                            value={listing.roomType}
+                            name="room_type"
+                            value={listing.room_type}
                             onChange={handleChange}
                             required
                             >
                                 <option value="none">What describes the space?</option>
-                                <option value="privateRoom">Private Room</option>
-                                <option value="entireHomeOrApt">Entire Home/Apt.</option>
+                                <option value="private_room">Private Room</option>
+                                <option value="entire_home_or_apt">Entire Home/Apt.</option>
                                 <option value="shared">Shared Home/Apt.</option>
                             </select>    
                         </label>   
@@ -216,32 +259,32 @@ const cancelForm = () => {
                         <label className="addListing-label">
                         <h4 className="label-text">Property Type: </h4>
                             <select
-                            name="propertyType"
-                            value={listing.propertyType}
+                            name="property_type"
+                            value={listing.property_type}
                             onChange={handleChange}
                             required
                             >
                                 <option value="none">What is the type of your listing?</option>
-                                <option value="House">House</option>
-                                <option value="Townhouse">Townhouse</option>
-                                <option value="Apartment">Apartment</option>
-                                <option value="Condominium">Condominium</option>
-                                <option value="Loft">Loft</option>
-                                <option value="ServicedApartment">Serviced Apartment</option> 
-                                <option value="Hostel">Hostel</option>
-                                <option value="guestSuite">Guest Suite</option>
-                                <option value="BedandBreakfast">Bed and Breakfast</option>
-                                <option value="guestHouse">Guest House</option>
+                                <option value="house">House</option>
+                                <option value="townhouse">Townhouse</option>
+                                <option value="apartment">Apartment</option>
+                                <option value="condominium">Condominium</option>
+                                <option value="loft">Loft</option>
+                                <option value="serviced_apartment">Serviced Apartment</option> 
+                                <option value="hostel">Hostel</option>
+                                <option value="guest_suite">Guest Suite</option>
+                                <option value="bed_and_breakfast">Bed and Breakfast</option>
+                                <option value="guest_house">Guest House</option>
                                 <option value="hotel">Hotel</option>
-                                <option value="boutiqueHotel">Boutique Hotel</option>
+                                <option value="boutique_hotel">Boutique Hotel</option>
                                 <option value="bungalow">Bungalow</option>
                                 <option value="boat">Boat</option>
-                                <option value="tinyHouse">Tiny House</option>
-                                <option value="houseBoat">House Boat</option>
-                                <option value="CamperRV">Camper/RV</option>
+                                <option value="tiny_house">Tiny House</option>
+                                <option value="house_boat">House Boat</option>
+                                <option value="camper_rv">Camper/RV</option>
                                 <option value="villa">Villa</option>
-                                <option value="aparthotel">Aparthotel</option>
-                                <option value="Other">Other</option>
+                                <option value="apart_hotel">Aparthotel</option>
+                                <option value="other">Other</option>
                             </select>    
                         </label>  
                     </Card>  
@@ -250,10 +293,10 @@ const cancelForm = () => {
                         <h4 className="label-text">Zip Code: </h4>
                             <input
                             type="number"
-                            name="zipCode"
+                            name="zip_code"
                             min="5"
                             placeholder="Zip Code"
-                            value={listing.zipCode}
+                            value={listing.zip_code}
                             onChange={handleChange}
                             required
                             />
