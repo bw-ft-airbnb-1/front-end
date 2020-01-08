6c788243c900006ebfd64b1b47ef79f4a907f197
Robert J. Mitchell <robert.judson@outlook.com>
2020-01-07 21:44:48 -0500

    Listing page and Card Creator built

--- optimal-price/src/components/Dashboard/MyListings.js
+++ optimal-price/src/components/Dashboard/MyListings.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavBar } from '../NavBar.js';
+import { Cards } from './CardCreator';
+import Axios from 'axios';
 
-export const MyListings = () => {
+export const MyListings = (props) => {
 
+    const [state, setState] = useState([])
+
+    useEffect(() => {
+        Axios
+            .get()
+            .then(response => {
+                setState(response.data)
+            })
+        .catch(error => console.log({error}))
+    },[])
 
     return(
         <div>
         <NavBar />
             My Listings
+        {state.map(()=> (
+            <Cards />
+        ))}
+ 
         </div>
     )
 }
